e301f1535c665b78bdb31d1c7623c6f685f9429e
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-21 14:12:39 -0400

    added survey.js with questions

--- survey.js
+++ survey.js
@@ -5,8 +5,19 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("What do you think of Node.js? ", (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
+rl.question("What's your name? Nicknames are also acceptable! :) ", (answer1) => {
+  rl.question("What's an activity you like doing?", (answer2) => {
+    rl.question("What do you listen to while doing that?", (answer3) => {
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)", (answer4) => {
+        rl.question("What's your favourite thing to eat for that meal?", (answer5) => {
+          rl.question("Which sport is your ABSOLUTE favourite?", (answer6) => {
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at!", (answer7) => {
+              console.log(`This is your profile: ${answer1} loves listening to ${answer3} while doing/playing ${answer2}, eating ${answer5} for ${answer4}, loves ${answer6}, and is amazing at ${answer7}.`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  }); 
 });
